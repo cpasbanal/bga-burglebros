4f8606de43e4f9a4c51ed894cf09d488a9a20910
cpasbanal <jcroyere@gmail.com>
2021-06-10 18:44:30 +0200

    FIx bug #40552 -- can no longer disclose the other service duct tile if not flipped

--- burglebros.game.php
+++ burglebros.game.php
@@ -709,8 +709,9 @@ SQL;
             $painting = $this->getPlayerLoot('painting', $current_player_id);
             $secret_door = $same_floor && $adjacent && $tile['type'] == 'secret-door';
             // Check $tile and $other_tile are different to avoid counting a move when clicking on service_duct
-            $service_duct = $tile['type'] == 'service-duct' && $other_tile['type'] == 'service-duct' && $tile['id'] != $other_tile['id'];
-            // TODO: This allows you to guess and reveal the other
+            // Check destination tile is flipped to avoid disclosing the other service duct card
+            $flipped = $this->getFlippedTiles($other_tile['location'][5]);
+            $service_duct = $tile['type'] == 'service-duct' && $other_tile['type'] == 'service-duct' && $tile['id'] != $other_tile['id'] && isset($flipped[$tile['id']]);
             if ($painting && (($secret_door && $blocked) || $service_duct)) {
                 throw new BgaUserException(self::_('Cannot move this way while holding the Painting'));
             }
